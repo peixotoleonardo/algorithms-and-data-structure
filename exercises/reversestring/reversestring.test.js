1b9cb29178c122a45c6fc6a8dbdcceb50e13c1d4
Leonardo <leonardo.peixoto@fortics.com.br>
2020-10-06 08:26:46 -0300

    test: add new exercise to reverse a string

---
+++ exercises/reversestring/reversestring.test.js
@@ -0,0 +1,19 @@
+const reversestring = require('./reversestring');
+
+describe('test function reversestringSolution1', () => {
+  test('given apple should return elppa', () => {
+    expect(reversestring.reversestringSolution1('apple')).toBe('elppa');
+  });
+});
+
+describe('test function reversestringSolution2', () => {
+  test('given apple should return elppa', () => {
+    expect(reversestring.reversestringSolution2('apple')).toBe('elppa');
+  })
+});
+
+describe('test function reversestringSolution3', () => {
+  test('given apple should return elppa', () => {
+    expect(reversestring.reversestringSolution3('apple')).toBe('elppa');
+  });
+});
