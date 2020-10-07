e8c27d2b7b5274b97e46aa5a4771c797de1d4184
Leonardo <leonardo.peixoto@fortics.com.br>
2020-10-07 07:17:17 -0300

    test: improve test

--- exercises/reversestring/reversestring.test.js
+++ exercises/reversestring/reversestring.test.js
@@ -1,19 +1,11 @@
 const reversestring = require('./reversestring');
 
-describe('test function reversestringSolution1', () => {
-  test('given apple should return elppa', () => {
-    expect(reversestring.reversestringSolution1('apple')).toBe('elppa');
-  });
-});
+Object.entries(reversestring).forEach((solution) => {
+  const [name, func] = solution;
 
-describe('test function reversestringSolution2', () => {
-  test('given apple should return elppa', () => {
-    expect(reversestring.reversestringSolution2('apple')).toBe('elppa');
-  })
-});
-
-describe('test function reversestringSolution3', () => {
-  test('given apple should return elppa', () => {
-    expect(reversestring.reversestringSolution3('apple')).toBe('elppa');
+  describe(`test function ${name}`, () => {
+    test('given apple should return elppa', () => {
+      expect(func('apple')).toBe('elppa');
+    });
   });
 });
