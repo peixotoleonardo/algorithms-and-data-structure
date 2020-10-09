df0d3415bb53b94d82bbca1e43998a57f5b532e7
Leonardo <leonardo.peixoto@fortics.com.br>
2020-10-09 07:09:50 -0300

    feat: add new exercise max char

---
+++ exercises/maxchars/maxchars.js
@@ -0,0 +1,39 @@
+/**
+ * Given a string, return the character that is most
+ * commonly used in the string
+ */
+
+const maxChar = (str) => {
+    let maxChar = '';
+    let max = 0;
+
+    const charMap = [...str].reduce((chars, char) => {
+        chars[char] = chars[char] + 1 || 1;
+
+        return chars;
+    }, {});
+
+    for (char in charMap) {
+        if (charMap[char] > max) {
+            max = charMap[char];
+            maxChar = char
+        }
+    }
+
+    return maxChar;
+}
+
+//     str.split('').reduce((char, chars) => {
+//     if (char in chars) {
+//         ++chars[char];
+//     } else {
+//         chars[char] = 1;
+//     }
+
+//     return chars;
+// }, {});
+
+module.exports = {
+    maxChar
+}
+
